bfd94564e59c030d4bf4f3597043e1f05c7daf78
Lucas Godoy <lucasgodoyjor@gmail.com>
2025-05-08 10:31:37 -0300

    hotfix mainmenu baord => board

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,9 @@ repositories {
 dependencies {
     implementation("org.liquibase:liquibase-core:4.29.1")
     implementation("mysql:mysql-connector-java:8.0.33")
-    implementation("org.projectlombok:lombok:1.18.34")
+    implementation("org.projectlombok:lombok:1.18.38")
 
-    annotationProcessor("org.projectlombok:lombok:1.18.34")
+    annotationProcessor("org.projectlombok:lombok:1.18.38")
 }
 
 tasks.test {
