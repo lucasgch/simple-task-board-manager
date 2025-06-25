df6d5f3a2d365d396b48efa31b572f2a02639842
Lucas Godoy <lucasgodoyjor@gmail.com>
2025-06-25 09:51:40 -0300

    Renamed class

--- src/test/java/org/desviante/test/TestLoadBoardsFromDatabase.java
+++ src/test/java/org/desviante/test/TestLoadBoardsFromDatabase.java
@@ -10,7 +10,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-public class testLoadBoardsFromDatabase {
+public class TestLoadBoardsFromDatabase {
 
     @Test
     void testLoadBoardsFromDatabase() throws SQLException {
